0ec6efdb876106b2f636e7afddb244711a7e0428
haniffadhil <50096560+potatoeee@users.noreply.github.com>
2025-06-24 16:22:44 +0800

    Add files via upload

--- labels.py
+++ labels.py
@@ -1,41 +1,106 @@
 PI_IPS = {
-    "row1": "http://172.18.82.5:5000",  
-    "row2": "http://172.18.82.6:5000",
+    "row1": "http://172.18.82.1:5000",  
+    "row2": "http://172.18.82.2:5000",
+    "row3": "http://172.18.82.3:5000",
     "row4": "http://172.18.82.4:5000",
-    "row5": "http://172.18.82.7:5000"
+    "row5": "http://172.18.82.5:5000",
+    "row6": "http://172.18.82.6:5000",
+    "row7": "http://172.18.82.7:5000",
+    "row8": "http://172.18.82.8:5000",
+    "row9": "http://172.18.82.9:5000",
+    "row10": "http://172.18.82.10:5000",
+    "row11": "http://172.18.82.11:5000",
+    "row12": "http://172.18.82.12:5000",
+    "row13": "http://172.18.82.13:5000"
 }
 
 FALLBACK_SCOPE_LABELS = {
     "row1": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
     "row2": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row3": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
     "row4": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
-    "row5": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"]
+    "row5": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row6": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row7": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row8": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row9": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row10": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row11": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row12": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"],
+    "row13": ["Scope 0", "Scope 1", "Scope 2", "Scope 3"]
 }
 
 # Updated serial to label mapping
 SERIAL_TO_LABEL = {
     "row1": {
-        "GEY180247": "H5",
-        "GEY180248": "H6",
-        "GEY180255": "G5",
-        "GES141341": "G6"
+        "":"",
+        "":"",
+        "":"",
+        "":""
     },
     "row2": {
-        "GEY180245": "H3",
-        "GEY180251": "G3",
-        "GEY180246": "H4",
-        "GEY180252": "G4"
+        "":"",
+        "":"",
+        "":"",
+        "":""
     },
     "row4": {
-        "GES854611": "F5",
-        "GES854606": "E5",
-        "GES854615": "F6",
-        "GES854612": "E6"
+        "":"",
+        "":"",
+        "":"",
+        "":""
     },
     "row5": {
-        "GES141338": "F3",
-        "GES141335": "E3",
-        "GES854607": "F4",
-        "GES141334": "E4"
+        "":"",
+        "":"",
+        "":"",
+        "":""
     },
+    "row6": {
+        "":"",
+        "":"",
+        "":"",
+        "":""
+    },
+    "row7": {
+        "":"",
+        "":""
+    },
+    "row8": {
+        "":"",
+        "":"",
+        "":"",
+        "":""
+    },
+    "row9": {
+        "GES141335": "E3",
+        "GES141334": "E4",
+        "GES141338": "F3",
+        "GES854607": "F4"
+    },
+    "row10": {
+        "GES854606": "E5",
+        "GES854612": "E6",
+        "GES854611": "F5",
+        "GES854615": "F6"
+    },
+    "row11": {
+        "":"",
+        "":"",
+        "":"",
+        "":""
+    },
+    "row12": {
+        "GEY180251": "G3",
+        "GEY180252": "G4",
+        "GEY180245": "H3",
+        "GEY180246": "H4"
+   
+    },
+    "row13": {
+        "GEY180255": "G5",
+        "GES141341": "G6",
+        "GEY180247": "H5",
+        "GEY180248": "H6"
+    }
 }
