7b9904ea7769105dd8f64b7ffaa63ac9b5bf1650
ong-kok-peng <darudestorm77@gmail.com>
2025-07-01 17:18:23 +0800

    updated GUI program

--- osc_ctrl_gui.py
+++ osc_ctrl_gui.py
@@ -1,59 +1,122 @@
 import tkinter as tk
 from PIL import Image, ImageTk
 import osc_ctrl_logic as ocl
+import labels #all the oscilloscope and oscilloscope groups definitions
+
+#global variables limited to within the oscilloscope gui only
 
 channel_on_states = [False, False, False, False]
-scope_running = False
 
 canvas = None
 sel_listbox = None
 sel_osc_grps_box = None; sel_oscs_box = None
-
+screen_console = None
 
 # ─────────────────────────────
 #  Callbacks
 # ─────────────────────────────
+def autoset():
+    screen_console.config(state=tk.NORMAL); screen_console.delete("1.0", tk.END)
+    
+    if len(ocl.selected_oscs) != 0: screen_console.insert(tk.END, "Appling AUTOSET for oscilloscope(s)...\n")
+    else: screen_console.insert(tk.END, "No oscilliscopes selected.\n")
+
+    for osc_grp in ocl.selected_oscs.keys():
+        screen_console.insert(tk.END, f"{ocl.selected_oscs[osc_grp]} in {osc_grp}\n")
+
+    #autoset control logic code
+
+    screen_console.config(state=tk.DISABLED)
+
+
 def default():
-    print("Default settings applied!\n")
+    screen_console.config(state=tk.NORMAL); screen_console.delete("1.0", tk.END)
+    
+    if len(ocl.selected_oscs) != 0: screen_console.insert(tk.END, "Appling default settings for oscilloscope(s)...\n")
+    else: screen_console.insert(tk.END, "No oscilliscopes selected.\n")
+
+    for osc_grp in ocl.selected_oscs.keys():
+        screen_console.insert(tk.END, f"{ocl.selected_oscs[osc_grp]} in {osc_grp}\n")
+
+    #default control logic code
+
+    screen_console.config(state=tk.DISABLED)
 
 def proficiency_test():
-    print("Proficiency test settings applied!\n")
+    screen_console.config(state=tk.NORMAL); screen_console.delete("1.0", tk.END)
+    
+    if len(ocl.selected_oscs) != 0: screen_console.insert(tk.END, "Appling Proficiency Test settings macro for oscilloscope(s)...\n")
+    else: screen_console.insert(tk.END, "No oscilliscopes selected.\n")
 
-def rescan_oscs():
-    print("Re-scanning for oscilloscopes...\n")
-    scan_connected_oscs()
+    for osc_grp in ocl.selected_oscs.keys():
+        screen_console.insert(tk.END, f"{ocl.selected_oscs[osc_grp]} in {osc_grp}\n")
 
-def autoset():
-    print("Oscilloscope auto-set done!\n")
+    #proficiency test control logic code
+
+    screen_console.config(state=tk.DISABLED)
+
+def scan_connected_oscs():
+    screen_console.config(state=tk.NORMAL); screen_console.delete("1.0", tk.END)
+
+    screen_console.insert(tk.END, "Scanning connected oscilloscope groups...\n")
+    connected_oscs_grps = ocl.scan_Oscs_grps()
+    oscs_frm_connected_grps = ocl.scan_Oscs(connected_oscs_grps)
+
+    if (len(connected_oscs_grps) != 0):
+        screen_console.insert(tk.END, "Found connected oscilloscope groups!\n")
+        sel_osc_grps_box.config(state=tk.NORMAL); sel_oscs_box.config(state=tk.NORMAL)
+
+        sel_osc_grps_box.delete(0, tk.END); sel_oscs_box.delete(0, tk.END) #reset both select listboxes
+
+        sel_osc_grps_box.insert(tk.END, *connected_oscs_grps) #populate the individual oscilloscope groups
+        sel_oscs_box.insert(tk.END, *oscs_frm_connected_grps) #populate the individual oscilloscopes
+
+        sel_osc_grps_box.config(state=tk.DISABLED); sel_oscs_box.config(state=tk.DISABLED)
+    else:
+        screen_console.insert(tk.END, "No oscilloscope groups found!\n")
+    screen_console.config(state=tk.DISABLED)
+
+def add_oscs_selection(event):
+    ocl.selected_oscs = {}
+    select_box = event.widget
+    selected_indices = select_box.curselection()
+
+    if sel_listbox.get() == 1 and len(selected_indices) > 0:
+        #append all the oscilloscopes of selected groups
+        for index in selected_indices:
+            osc_grp_id = select_box.get(index)
+            ocl.selected_oscs[osc_grp_id] = [] #put each group first as key
+            for osc_id in labels.SERIAL_TO_LABEL[osc_grp_id].values():
+                ocl.selected_oscs[osc_grp_id].append(osc_id) #put all the oscilloscope ids into each group
+
+    elif sel_listbox.get() == 2 and len(selected_indices) > 0:
+        #append selected oscilloscopes of the respective groups
+        for index in selected_indices:
+            osc_id = select_box.get(index)
+            osc_id = osc_id[0:osc_id.find('(')-1] #removes the space before and brackets substring from the selected value
+            for osc_grp_id in labels.SERIAL_TO_LABEL.keys():
+                if osc_id in labels.SERIAL_TO_LABEL[osc_grp_id].values(): #find the corresponding group of the selected oscilloscope
+                    if osc_grp_id not in ocl.selected_oscs: ocl.selected_oscs[osc_grp_id] = [] #if key not exist create the group key
+                    ocl.selected_oscs[osc_grp_id].append(osc_id)   
+
+    #print(ocl.selected_oscs) 
 
 def toggle_sel_listbox():
-    #print(f"Radio button {sel_listbox.get()} is clicked.")
     if sel_listbox.get() == 1:
+        #select oscilloscope groups
         if sel_osc_grps_box['state'] == tk.DISABLED:
-            sel_osc_grps_box.config(state=tk.NORMAL)
+            sel_osc_grps_box.config(state=tk.NORMAL); sel_osc_grps_box.bind('<<ListboxSelect>>', add_oscs_selection)
         if sel_oscs_box['state'] == tk.NORMAL:
-            sel_oscs_box.config(state=tk.DISABLED)
+            sel_oscs_box.selection_clear(0, tk.END)
+            sel_oscs_box.config(state=tk.DISABLED); sel_oscs_box.unbind('<<ListboxSelect>>')
 
     elif sel_listbox.get() == 2:
+        #select oscilliscopes individually 
         if sel_oscs_box['state'] == tk.DISABLED:
-            sel_oscs_box.config(state=tk.NORMAL)
+            sel_oscs_box.config(state=tk.NORMAL); sel_oscs_box.bind('<<ListboxSelect>>', add_oscs_selection)
         if sel_osc_grps_box['state'] == tk.NORMAL:
-            sel_osc_grps_box.config(state=tk.DISABLED)
-
-def toggle_run_stop(button):
-    global scope_running
-
-    try:
-        if scope_running:
-            button.config(text="Stop", bg="#e32727")
-            print("Oscilloscope has stopped running.\n")
-        else:
-            button.config(text="Run", bg="#57d459")
-            print("Oscilloscope is running.\n")
-
-        scope_running = not scope_running
-    except Exception as e:
-        print("Error toggling run/stop:", e)
+            sel_osc_grps_box.selection_clear(0, tk.END)
+            sel_osc_grps_box.config(state=tk.DISABLED); sel_osc_grps_box.unbind('<<ListboxSelect>>')
 
 def toggle_channel(ch_index, button):
     global channel_on_states
@@ -111,10 +174,9 @@ def build_background(root):
     canvas.create_image(0, 0, anchor=tk.NW, image=canvas.image)
 
 def build_osc_buttons(root):
-    #create autoset, run and default buttons
-    run_btn = tk.Button(root, text="Run", command=lambda: toggle_run_stop(run_btn), bg="lightgray", relief="raised", width=8)
-    canvas.create_window(1150, 90, window=run_btn)
+    #NOTE: All buttons position bounds are based on center point
 
+    #create autoset and default buttons
     add_button(1150, 180, "Default", default, "lightgray")
     add_button(1150, 50, "Autoset", autoset, "lightgray")
 
@@ -125,6 +187,21 @@ def build_osc_buttons(root):
     create_channel_button(4, 1047, 379)
 
 def build_widgets(root):
+    #NOTE: All UI widgets position bounds are based on center point
+
+    #screen text console (overlay of oscilloscope screen)
+    global screen_console
+    screen_console_frame = tk.Frame(root, width=470, height=200)
+    screen_console_frame.pack_propagate(False) # Prevent frame from resizing to content
+    canvas.create_window(300, 320, window=screen_console_frame)
+
+    screen_console = tk.Text(screen_console_frame, state=tk.DISABLED)
+    screen_console.pack()
+
+    #macro panel
+    add_label(300, 450, "Preset Macro Settings", 15, "white", "#453A42")
+    add_button(88, 485, "Proficiency", proficiency_test, "orange")
+
     #oscilloscope groups selection
     osc_grps_frame = tk.Frame(root, width=370, height=80)
     osc_grps_frame.pack_propagate(False) # Prevent frame from resizing to content
@@ -138,31 +215,11 @@ def build_widgets(root):
     global sel_osc_grps_box, sel_oscs_box
     sel_osc_grps_box = tk.Listbox(osc_grps_frame, width=30, height=4, selectmode=tk.MULTIPLE, state=tk.DISABLED)
     sel_osc_grps_box.grid(row=1, column=0)
+
     sel_oscs_box = tk.Listbox(osc_grps_frame, width=30, height=4, selectmode=tk.MULTIPLE, state=tk.DISABLED)
     sel_oscs_box.grid(row=1, column=1, padx=5)
 
-    add_button(65, 585, "Re-Scan\noscillo-\nscopes", rescan_oscs, "lightgray")
-
-    #macro panel
-    add_label(300, 450, "Preset Macro Settings", 15, "white", "#453A42")
-    add_button(88, 485, "Proficiency", proficiency_test, "orange")
-
-# ────────────────────────────────
-#  Scan connected oscilloscopes for first time and subsequently
-# ────────────────────────────────
-def scan_connected_oscs():
-    connected_oscs_grps = ocl.scan_Oscs_grps()
-    oscs_frm_connected_grps = ocl.scan_Oscs(connected_oscs_grps)
-
-    if (len(connected_oscs_grps) != 0):
-        print("Found connected oscilloscope groups!\n")
-        sel_osc_grps_box.config(state=tk.NORMAL); sel_oscs_box.config(state=tk.NORMAL)
-
-        sel_osc_grps_box.delete(0, tk.END); sel_oscs_box.delete(0, tk.END)
-        sel_osc_grps_box.insert(tk.END, *connected_oscs_grps)
-        sel_oscs_box.insert(tk.END, *oscs_frm_connected_grps)
-
-        sel_osc_grps_box.config(state=tk.DISABLED); sel_oscs_box.config(state=tk.DISABLED)
+    add_button(65, 585, "Re-Scan\noscillo-\nscopes", scan_connected_oscs, "lightgray")
     
 # ─────────────────────────────
 #  Program main function
@@ -175,7 +232,7 @@ def main() -> None:
     build_background(root)
     build_osc_buttons(root)
     build_widgets(root)
-    scan_connected_oscs() 
+    scan_connected_oscs() #scan oscilloscopes for first time
     root.mainloop()
 
 if __name__ == "__main__":
--- osc_ctrl_logic.py
+++ osc_ctrl_logic.py
@@ -1,49 +1,21 @@
 import requests
+import labels #all the oscilloscope and oscilloscope groups definitions
 
 #general global variables
 
-PI_IPS = {
-    "Row G5-H5-G6-H6": "http://172.18.82.6:5000",
-    "Row G3-H3-G4-H4": "http://172.18.82.5:5000",
-    "Row F5-E5-F6-E6": "http://172.18.82.4:5000",
-    "Row F3-E3-F4-E4": "http://172.18.82.7:5000"
-}
+selected_oscs = {}
 
-# Updated serial to label mapping
-SERIAL_TO_LABEL = {
-    "Row G5-H5-G6-H6": {
-        "GEY180247": "H5",
-        "GEY180248": "H6",
-        "GEY180255": "G5",
-        "GES141341": "G6"
-    },
-    "Row G3-H3-G4-H4": {
-        "GEY180245": "H3",
-        "GEY180251": "G3",
-        "GEY180246": "H4",
-        "GEY180252": "G4"
-    },
-    "Row F5-E5-F6-E6": {
-        "GES854611": "F5",
-        "GES854606": "E5",
-        "GES854615": "F6",
-        "GES854612": "E6"
-    },
-    "Row F3-E3-F4-E4": {
-        "GES141338": "F3",
-        "GES141335": "E3",
-        "GES854607": "F4",
-        "GES141334": "E4"
-    },
-}
+#logic functions
 
 def scan_Oscs_grps():
     #scan for oscilloscope groups that are connected
     connected_Osc_grps = []
-    available_Osc_grps = list(PI_IPS.keys())
+    available_Osc_grps = list(labels.PI_IPS.keys())
     
     connected_Osc_grps.append(available_Osc_grps[1])
     connected_Osc_grps.append(available_Osc_grps[3])
+    connected_Osc_grps.append(available_Osc_grps[8])
+    connected_Osc_grps.append(available_Osc_grps[9])
 
     return connected_Osc_grps
 
@@ -52,7 +24,8 @@ def scan_Oscs(connected_Osc_grps):
     available_Oscs = []
 
     for row_id in connected_Osc_grps:
-        for osc_id in SERIAL_TO_LABEL[row_id]:
-            available_Oscs.append(SERIAL_TO_LABEL[row_id][osc_id])
+        for osc_id in labels.SERIAL_TO_LABEL[row_id].values():
+            osc_id_string = f"{osc_id} ({row_id})"
+            available_Oscs.append(osc_id_string)
 
     return available_Oscs
