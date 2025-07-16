091a45de8686eaf44846400d45bd653e20cde8ca
ong-kok-peng <darudestorm77@gmail.com>
2025-07-16 11:52:49 +0800

    added GUI's default and proficiency test thread tasks

--- OscilloscopeBgWorkers.py
+++ OscilloscopeBgWorkers.py
@@ -13,11 +13,15 @@ def scan_connected_oscs(functionName):
     #scan for oscilloscope groups that are connected
     taskStatus[functionName] = "running"
     oscilloscopes = {"oscilloscope groups": [], "oscilloscope ids": []}
-    
-    connected_oscilloscope_groups_count = 0
-    taskStatusQueue.put((functionName, "Scanning for oscilloscope row(s)...\n"))
 
-    for group in labels.PI_IPS.keys():
+    #uncomment the below to test the oscilloscope functions if scanning oscilloscopes is impossible (bypass the scanning)
+    #oscilloscopes_test = {"oscilloscope groups": ["row1", "row5"], 
+    #                      "oscilloscope ids": ["A1 (row1)", "A2 (row1)", "B1 (row1)", "B2 (row1)",
+    #                                           "C3 (row5)", "C4 (row5)", "D3 (row5)", "D4 (row5)"]}
+    
+    taskStatusQueue.put((functionName, "Scanning for oscilloscope group(s)...\n\n"))
+
+    for group in labels.PI_IPS.keys(): # loop through all the 13 group IPs, and check if their HTTP server respond
         try:
             res = requests.get(f"{labels.PI_IPS[group]}/", timeout=2)
             if res.status_code == 200:
@@ -29,17 +33,17 @@ def scan_connected_oscs(functionName):
                     osc_id_string = f"{osc_id} ({group})"
                     oscilloscopes["oscilloscope ids"].append(osc_id_string)
                 
-                connected_oscilloscope_groups_count += 1
             else:
                 taskStatusQueue.put((functionName, f"❌ Oscilloscope group {group} is not connected; with error {res.status_code}.\n"))
         except Exception:
             taskStatusQueue.put((functionName, f"❌ Oscilloscope group {group} is not connected; connection timed out.\n"))
 
-    if connected_oscilloscope_groups_count > 0:
+    if len(oscilloscopes["oscilloscope groups"]) > 0:
         taskStatusQueue.put((functionName, oscilloscopes))
-        taskStatusQueue.put((functionName, f"{connected_oscilloscope_groups_count} oscilloscope group(s) found!\n"))
+        taskStatusQueue.put((functionName, f"{len(oscilloscopes["oscilloscope groups"])} oscilloscope group(s) found!\n"))
     else:
         taskStatusQueue.put((functionName, "Finished scanning; no connected oscilloscope groups found!\n"))
+        #taskStatusQueue.put((functionName, oscilloscopes_test)) #uncomment this if bypassing scanning to test oscilloscope functions
 
     taskStatusQueue.put((functionName, "done"))
     taskStatus[functionName] = "stopped"
@@ -50,7 +54,7 @@ def autoset(functionName):
         taskStatusQueue.put((functionName, "Applying AUTOSET for oscilloscope(s)...\n\n"))
 
         for oscilloscope_grp in selected_oscs.keys():
-            taskStatusQueue.put((functionName, f"In group {oscilloscope_grp}:\n"))
+            taskStatusQueue.put((functionName, f"In group {oscilloscope_grp}:\n")) # print this at the start of processing every group
 
             for oscilloscope_label in selected_oscs[oscilloscope_grp]:
                 try:
@@ -60,10 +64,10 @@ def autoset(functionName):
                         taskStatusQueue.put((functionName, f"✅ AUTOSET command sent to {oscilloscope_label} successfully!\n"))
                     elif res.json().get("status", "?") == "error":
                         err_msg = res.json().get("message", "?")
-                        taskStatusQueue.put((functionName, f"❌ Unable to AUTOSET {oscilloscope_label} because {err_msg}.\n"))
+                        taskStatusQueue.put((functionName, f"❌ Failed to send AUTOSET to {oscilloscope_label} because {err_msg}.\n"))
 
                 except Exception:
-                    taskStatusQueue.put((functionName, f"🚫: An exception occured when sending AUTOSET to {oscilloscope_label}.\n"))
+                    taskStatusQueue.put((functionName, f"🚫: Exception occured when sending AUTOSET to {oscilloscope_label}.\n"))
             taskStatusQueue.put((functionName, "\n")) # print newline in the console after every group is done
 
         taskStatusQueue.put((functionName, "Finished applying AUTOSET for oscilloscope(s)!\n"))
@@ -77,13 +81,26 @@ def autoset(functionName):
 def default(functionName):
     taskStatus[functionName] = "running"
     if len(selected_oscs) != 0: 
-        taskStatusQueue.put((functionName, "Applying DEAFULT for oscilloscope(s)...\n"))
-        for osc_grp in selected_oscs.keys():
-            taskStatusQueue.put((functionName, f"{selected_oscs[osc_grp]} in {osc_grp}\n"))
+        taskStatusQueue.put((functionName, "Applying DEAFULT for oscilloscope(s)...\n\n"))
 
-            sleep(5)
-            #autoset control code
-            taskStatusQueue.put((functionName, "Finished applying DEFAULT for oscilloscope(s)!\n"))
+        for oscilloscope_grp in selected_oscs.keys():
+            taskStatusQueue.put((functionName, f"In group {oscilloscope_grp}:\n")) # print this at the start of processing every group
+
+            for oscilloscope_label in selected_oscs[oscilloscope_grp]:
+                try:
+                    res = requests.get(f"{labels.PI_IPS[oscilloscope_grp]}/default", params={"label": oscilloscope_label}, timeout=3)
+                    
+                    if res.json().get("status", "?") == "success":
+                        taskStatusQueue.put((functionName, f"✅ DEFAULT command sent to {oscilloscope_label} successfully!\n"))
+                    elif res.json().get("status", "?") == "error":
+                        err_msg = res.json().get("message", "?")
+                        taskStatusQueue.put((functionName, f"❌ Failed to send DEFAULT to {oscilloscope_label} because {err_msg}.\n"))
+
+                except Exception:
+                    taskStatusQueue.put((functionName, f"🚫: Exception occured when sending DEFAULT to {oscilloscope_label}.\n"))
+            taskStatusQueue.put((functionName, "\n")) # print newline in the console after every group is done
+
+        taskStatusQueue.put((functionName, "Finished applying DEFAULT for oscilloscope(s)!\n"))
 
     else: taskStatusQueue.put((functionName, "No oscilloscopes selected.\n"))
 
@@ -93,13 +110,27 @@ def default(functionName):
 def proficiency_test(functionName):
     taskStatus[functionName] = "running"
     if len(selected_oscs) != 0: 
-        taskStatusQueue.put((functionName, "Applying proficiency test macro settings for oscilloscope(s)...\n"))
-        for osc_grp in selected_oscs.keys():
-            taskStatusQueue.put((functionName, f"{selected_oscs[osc_grp]} in {osc_grp}\n"))
+        taskStatusQueue.put((functionName, "Applying proficiency test macro for oscilloscope(s)...\n\n"))
 
-            sleep(5)
-            #autoset control code
-            taskStatusQueue.put((functionName, "Finished applying proficiency test macro settings for oscilloscope(s)!\n"))
+        for oscilloscope_grp in selected_oscs.keys():
+            taskStatusQueue.put((functionName, f"In group {oscilloscope_grp}:\n")) # print this at the start of processing every group
+
+            for oscilloscope_label in selected_oscs[oscilloscope_grp]:
+                try:
+                    res = requests.get(f"{labels.PI_IPS[oscilloscope_grp]}/proficiency", params={"label": oscilloscope_label}, timeout=3)
+                    
+                    if res.json().get("status", "?") == "success":
+                        taskStatusQueue.put((functionName, f"✅ Proficiency test set to {oscilloscope_label} successfully!\n"))
+                    elif res.json().get("status", "?") == "error":
+                        err_msg = res.json().get("message", "?")
+                        taskStatusQueue.put((functionName, f"❌ Failed to set proficiency test to {oscilloscope_label} because {err_msg}.\n"))
+
+                except Exception:
+                    taskStatusQueue.put((functionName, f"🚫: Exception occured when setting proficiency test to {oscilloscope_label}.\n"))
+            taskStatusQueue.put((functionName, "\n")) # print newline in the console after every group is done
+
+        taskStatusQueue.put((functionName, "Finished applying proficiency test macro for oscilloscope(s)!\n"))
+
 
     else: taskStatusQueue.put((functionName, "No oscilloscopes selected.\n"))
 
